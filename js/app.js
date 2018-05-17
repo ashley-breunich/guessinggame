167f5a60c83e51e17b989555e5dc0bb22b58b0cc
Ashley Breunich <ashleybreunich@gmail.com>
2018-05-16 23:30:56 -0700

    test

--- js/app.js
+++ js/app.js
@@ -13,117 +13,136 @@ console.log(userName);
 alert('Great, ' + userName + '. Let\'s begin!');
 
 // Question 1 - Cat Person
-var catPerson = prompt('Am I a cat person?' , 'type yes or no').toLowerCase();
+function questOne () {
+  var catPerson = prompt('Am I a cat person?' , 'type yes or no').toLowerCase();
 
-console.log(catPerson);
+  console.log(catPerson);
 
-if (catPerson === 'yes' || catPerson === 'y') {
-  alert('Excellent job, ' + userName + '. I am indeed OBSESSED with cats and have an adorable orange tabby named Ponyo.');
-  userPoints++;
-} else {
-  alert('I am definitely a cat person, ' + userName + ' and can\'t believe you would think otherwise!');
+  if (catPerson === 'yes' || catPerson === 'y') {
+    alert('Excellent job, ' + userName + '. I am indeed OBSESSED with cats and have an adorable orange tabby named Ponyo.');
+    userPoints++;
+  } else {
+    alert('I am definitely a cat person, ' + userName + ' and can\'t believe you would think otherwise!');
+  }
 }
+questOne();
 
 // Question 2 - Red Hair
-var redHair = prompt('Is red my natural hair color?' , 'type yes or no').toLowerCase();
+function questTwo () {
+  var redHair = prompt('Is red my natural hair color?' , 'type yes or no').toLowerCase();
 
-console.log(redHair);
+  console.log(redHair);
 
-if (redHair === 'yes' || redHair === 'y') {
-  alert('You are correct, ' + userName + '! I have never dyed my hair in my life.');
-  userPoints++;
-} else {
-  alert('Incorrect! I have never dyed my hair in my life.');
+  if (redHair === 'yes' || redHair === 'y') {
+    alert('You are correct, ' + userName + '! I have never dyed my hair in my life.');
+    userPoints++;
+  } else {
+    alert('Incorrect! I have never dyed my hair in my life.');
+  }
 }
+questTwo();
 
 // Question 3 - Snowboarder
-var snowBoard = prompt('Am I a snowboarder?' , 'type yes or no').toLowerCase();
+function questThree(){
+  var snowBoard = prompt('Am I a snowboarder?' , 'type yes or no').toLowerCase();
 
-console.log(snowBoard);
+  console.log(snowBoard);
 
-if (snowBoard === 'no' || snowBoard === 'n') {
-  alert('That\'s right, ' + userName + '! I prefer to ski my way down the slopes.');
-  userPoints++;
-} else {
-  alert('No way, ' + userName + '! I am a ski bum :)');
+  if (snowBoard === 'no' || snowBoard === 'n') {
+    alert('That\'s right, ' + userName + '! I prefer to ski my way down the slopes.');
+    userPoints++;
+  } else {
+    alert('No way, ' + userName + '! I am a ski bum :)');
+  }
 }
+questThree();
 
 // Question 4 - Morning Person
-var morningPerson = prompt('Am I a morning person?' , 'type yes or no').toLowerCase();
+function questFour() {
+  var morningPerson = prompt('Am I a morning person?' , 'type yes or no').toLowerCase();
 
-console.log(morningPerson);
+  console.log(morningPerson);
 
-if (morningPerson === 'no' || morningPerson === 'n') {
-  alert('You know me so well, ' + userName + '! I love sleep and dread waking up early.');
-  userPoints++;
-} else {
-  alert('No way, ' + userName + '. I love sleeping in.');
+  if (morningPerson === 'no' || morningPerson === 'n') {
+    alert('You know me so well, ' + userName + '! I love sleep and dread waking up early.');
+    userPoints++;
+  } else {
+    alert('No way, ' + userName + '. I love sleeping in.');
+  }
 }
+questFour ();
 
 // Question 5 - Favorite Cocktail
-var manhattan = prompt('Is my favorite cocktail a Manhattan?' , 'type yes or no').toLowerCase();
+function questFive (){
+  var manhattan = prompt('Is my favorite cocktail a Manhattan?' , 'type yes or no').toLowerCase();
 
-console.log(manhattan);
+  console.log(manhattan);
 
-if (manhattan === 'yes' || manhattan === 'y') {
-  alert('You guessed correct, ' + userName + '! My go-to order is a Manhattan with Maker\'s Mark.');
-  userPoints++;
-} else {
-  alert('Wrong! What\'s better than a Manhattan with Maker\'s Mark, ' + userName + '?');
+  if (manhattan === 'yes' || manhattan === 'y') {
+    alert('You guessed correct, ' + userName + '! My go-to order is a Manhattan with Maker\'s Mark.');
+    userPoints++;
+  } else {
+    alert('Wrong! What\'s better than a Manhattan with Maker\'s Mark, ' + userName + '?');
+  }
 }
+questFive();
 
 // Question 6 - How many states have I lived in?
-
-for (var i = 0; i < 4; i++) {
-  var states = prompt('In how many states have I lived? You have 4 chances to guess correctly!', 'type in a number');
-  console.log(states);
-  if (states === '4') {
-    alert('You are correct, ' + userName + '! I have lived in Connecticut, New York, California and Washington.');
-    userPoints++;
-    break;
-  } else if (states > 4) {
-    if (i < 3) {
-      alert('A little lower, ' + userName + '.');
+function questSix () {
+  for (var i = 0; i < 4; i++) {
+    var states = prompt('In how many states have I lived? You have 4 chances to guess correctly!', 'type in a number');
+    console.log(states);
+    if (states === '4') {
+      alert('You are correct, ' + userName + '! I have lived in Connecticut, New York, California and Washington.');
+      userPoints++;
+      break;
+    } else if (states > 4) {
+      if (i < 3) {
+        alert('A little lower, ' + userName + '.');
+      } else {
+        alert('Too bad! In case you were wondering, I have lived in 4 states: Connecticut, New York, California and Washington.');
+      }
     } else {
-      alert('Too bad! In case you were wondering, I have lived in 4 states: Connecticut, New York, California and Washington.');
-    }
-  } else {
-    if (i < 3) {
-      alert('A little higher, ' + userName + '.');
-    } else {
-      alert('Too bad! In case you were wondering, I have lived in 4 states: Connecticut, New York, California and Washington.');
+      if (i < 3) {
+        alert('A little higher, ' + userName + '.');
+      } else {
+        alert('Too bad! In case you were wondering, I have lived in 4 states: Connecticut, New York, California and Washington.');
+      }
     }
   }
 }
+questSix ();
 
 // Question 7 - Favorite Animal
+function cute() {
+  var favoriteAnimals = ['wolf', 'red panda', 'alpaca', 'otter']; // lists the animal array
+  var animal;
+  var counter = 0;
+  var rightAnswer;
 
-var favoriteAnimals = ['wolf', 'red panda', 'alpaca', 'otter']; // lists the animal array
-var animal;
-var counter = 0;
-var rightAnswer;
-
-while (counter < 7) {
-  animal = prompt('Can you guess one of my favorite animals? You have 6 chances to guess correctly.').toLowerCase();
-  console.log(animal); // prints the user's animal
-  for (i = 0; i < favoriteAnimals.length; i++) { // this loops through the array
-    console.log(favoriteAnimals[i]); // This prints all of the elements in the array
-    if (animal === favoriteAnimals[i]) { // If the user's animal matches any of the animals in the array
-      rightAnswer = true;
+  while (counter < 7) {
+    animal = prompt('Can you guess one of my favorite animals? You have 6 chances to guess correctly.').toLowerCase();
+    console.log(animal); // prints the user's animal
+    for (var i = 0; i < favoriteAnimals.length; i++) { // this loops through the array
+      console.log(favoriteAnimals[i]); // This prints all of the elements in the array
+      if (animal === favoriteAnimals[i]) { // If the user's animal matches any of the animals in the array
+        rightAnswer = true;
+      }
+    }
+    if (!rightAnswer) {
+      alert('Try again!');
+      counter++;
+    } else {
+      alert('Congratulations, ' + userName + ' you guessed one! My four favorite animals are a wolf, red panda, alpaca and otter.'); // congratulate them
+      counter += 7;
+      userPoints++; // give them a point
+    }
+    if (counter === 6) {
+      alert('No matches! In case you are interested, my four favorite animals are a wolf, red panda, alpaca and otter');
+      break;
     }
   }
-  if (!rightAnswer) {
-    alert('Try again!');
-    counter++;
-  } else {
-    alert('Congratulations, ' + userName + ' you guessed one! My four favorite animals are a wolf, red panda, alpaca and otter.'); // congratulate them
-    counter += 7;
-    userPoints++; // give them a point
-  }
-  if (counter === 6){
-    alert('No matches! In case you are interested, my four favorite animals are a wolf, red panda, alpaca and otter');
-    break;
-  }
 }
+cute ();
 
 alert('Thanks for playing my game, ' + userName + '! You earned ' + userPoints + ' points out of 7 total points.');
